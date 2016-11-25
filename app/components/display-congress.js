dbd9199988a79ac7ad7933a0e105e3555ef36929
Marie Chatfield <marie.chatfield@gmail.com>
2016-11-24 22:44:49 -0800

    Always show full URL for permalink.

--- app/components/display-congress.js
+++ app/components/display-congress.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
-import Config from 'call-my-congress/config/environment';
 
 export default Ember.Component.extend({
   congress: null,
 
   permalink: Ember.computed('congress.district.id', function() {
-    return `${Config.rootURL}${this.get('congress.district.id')}`;
+    return `http://www.callmycongress.com/${this.get('congress.district.id')}`;
   }),
 
   actions: {
