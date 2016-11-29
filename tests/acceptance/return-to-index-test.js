7cd122d5bcc1076fddaf646a1be4f92a29558540
Marie Chatfield <marie.chatfield@gmail.com>
2016-11-29 00:07:50 -0800

    Resolves #9. Click header to return to home page.

---
+++ tests/acceptance/return-to-index-test.js
@@ -0,0 +1,46 @@
+import { test } from 'qunit';
+import $ from 'jquery';
+import moduleForAcceptance from 'call-my-congress/tests/helpers/module-for-acceptance';
+import setupStubs from '../helpers/setup-stubs';
+
+moduleForAcceptance('Acceptance | return to index', {
+  beforeEach() {
+    this.stubs = setupStubs([
+      {
+        name: '$',
+        methodOverrides: [{
+          name: 'getJSON',
+          override: () => {
+            return {
+              catch() { return {}; }
+            };
+          }
+        }]
+      }
+    ]);
+
+    $.getJSON = this.stubs.objects.$.getJSON;
+  },
+
+  afterEach() {
+    $.getJSON = this.orginalGetJSON;
+  }
+});
+
+test('visiting index', function(assert) {
+  visit('/');
+  click('.header');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/', 'clicking header on index does not change URL');
+  });
+});
+
+test('visiting district', function(assert) {
+  visit('/CA-12');
+  click('.header');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/', 'clicking header on district page transitions to index');
+  });
+});
