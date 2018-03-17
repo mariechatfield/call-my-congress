4e8ce73f86b9ba9fe7940f79d9b0ec7859a6b936
Marie Chatfield <marie.chatfield@gmail.com>
2018-03-16 21:48:08 -0700

    Use ember-qunit directly instead of ember-cli-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,5 @@
 import resolver from './helpers/resolver';
-import { start } from 'ember-cli-qunit';
-import { setResolver } from 'ember-qunit';
+import { start, setResolver } from 'ember-qunit';
 
 setResolver(resolver);
 start();
