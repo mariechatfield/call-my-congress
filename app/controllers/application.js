bd9aae3f31f3fb943b0948ac64310d1907fc08e7
Marie Chatfield <marie.chatfield@gmail.com>
2018-07-04 11:14:10 -0700

    Make header link use semantic HTML
    Instead of leaving the header a regular div with some special
    click handling, use a proper `<a>` tag so that the element
    gets correct focus and treatment from assitive tech devices.
    
    Instead of doing a route transition, we can do a simple href
    to the index URL. Need to also introduce styling to make the
    header still maintain the styled look of a title (not a link)
    although I did add a hover shadow.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,11 +2,5 @@ import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  message: service(),
-
-  actions: {
-    linkToIndex() {
-      this.transitionToRoute('search');
-    }
-  }
+  message: service()
 });
